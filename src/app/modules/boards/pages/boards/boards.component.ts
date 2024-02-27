e99e557e5d77884b99b16c734d9c71b2f933565e
oscarbravo <oscarbravoarrieta@gmail.com>
2024-02-27 13:43:24 -0500

    Get boards

--- src/app/modules/boards/pages/boards/boards.component.ts
+++ src/app/modules/boards/pages/boards/boards.component.ts
@@ -1,24 +1,53 @@
-import { Component } from '@angular/core';
-import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
-import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { Component, inject, OnInit } from '@angular/core'
+import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faTrello } from '@fortawesome/free-brands-svg-icons'
+import { MeService } from '@services/me.service'
+import { Board } from '@models/board.model'
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html'
 })
-export class BoardsComponent {
+export class BoardsComponent implements OnInit  {
 
-  faTrello = faTrello;
-  faBox = faBox;
-  faWaveSquare = faWaveSquare;
-  faClock = faClock;
-  faAngleUp = faAngleUp;
-  faAngleDown = faAngleDown;
-  faHeart = faHeart;
-  faBorderAll = faBorderAll;
-  faUsers = faUsers;
-  faGear = faGear;
+     private meService = inject(MeService)
+     boards: Board[] = []
 
-  constructor() { }
+     faTrello = faTrello
+     faBox = faBox
+     faWaveSquare = faWaveSquare
+     faClock = faClock
+     faAngleUp = faAngleUp
+     faAngleDown = faAngleDown
+     faHeart = faHeart
+     faBorderAll = faBorderAll
+     faUsers = faUsers
+     faGear = faGear
+
+     //--------------------------------------------------------------------------------------------
+
+     constructor() { }
+
+     //--------------------------------------------------------------------------------------------
+
+     ngOnInit() {
+
+         this.getMyBoards()
+
+     }
+
+     //--------------------------------------------------------------------------------------------
+
+
+
+     getMyBoards() {
+
+         this.meService.getMeBoards().subscribe(boards => (
+             this.boards = boards
+         ))
+
+     }
+
+     //--------------------------------------------------------------------------------------------
 
 }
